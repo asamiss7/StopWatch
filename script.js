9fd33d52f152a92e6fcf9e6cf17eb25654dce828
asamiss7 <simasa678@gmail.com>
2020-08-30 11:34:01 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var startButton = document.querySelector(".start-btn");
+var stopButton = document.querySelector(".stop-btn");
+var resetButton = document.querySelector(".reset-btn");
+var theTimer = document.querySelector("#timer");
+
+var timer = [0,0,0,0];
+
+function leadingZero(time) {
+    if (time<=9) {
+        time= "0" + time;
+    }
+    return time;
+}
+
+//function to start the timer
+function runTimer() {
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + "." + leadingZero(timer[2]);
+    theTimer.innerHTML= currentTime;
+    timer[3]++;
+
+    timer[0]= Math.floor((timer[3]/100)/60); //for minutes
+    timer[1]= Math.floor((timer[3]/100) - (timer[0]*60)); //for seconds
+    timer[2]= Math.floor(timer[3] - (timer[1]*100) - (timer[0]*6000));
+}
+
+startButton.onclick = function () {
+    // clearInterval(interval);
+    interval = setInterval(runTimer,10);
+}
