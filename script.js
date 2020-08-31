ee06c8f2aded2d4a061095999e849a8a29aaa270
asamiss7 <simasa678@gmail.com>
2020-08-31 20:48:52 +0545

    add button to show lap values

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var startButton = document.querySelector(".start-btn");
 var stopButton = document.querySelector(".stop-btn");
 var resetButton = document.querySelector(".reset-btn");
+var lapButton = document.querySelector(".lap-btn");
 var theTimer = document.querySelector("#timer");
 
 var timer = [0,0,0,0];
 var interval;
+var i=1;
 
 function leadingZero(time) {
     if (time<=9) {
@@ -13,19 +15,19 @@ function leadingZero(time) {
     return time;
 }
 
-//function to start the timer
+//function to run the timer
 function runTimer() {
     let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + "." + leadingZero(timer[2]);
-    theTimer.innerHTML= currentTime;
+    theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0]= Math.floor((timer[3]/100)/60); //for minutes
-    timer[1]= Math.floor((timer[3]/100) - (timer[0]*60)); //for seconds
-    timer[2]= Math.floor(timer[3] - (timer[1]*100) - (timer[0]*6000));
+    timer[0] = Math.floor((timer[3]/100)/60); //for minutes
+    timer[1] = Math.floor((timer[3]/100) - (timer[0]*60)); //for seconds
+    timer[2] = Math.floor(timer[3] - (timer[1]*100) - (timer[0]*6000));
 }
 
 startButton.onclick = function () {
-    startButton.innerHTML= "Resume";
+    startButton.innerHTML = "Resume";
     clearInterval(interval);
     interval = setInterval(runTimer,10);
 }
@@ -34,9 +36,26 @@ stopButton.onclick = function () {
     clearInterval(interval);
 }
 
+// clear the timer
 resetButton.onclick = function () {
-    startButton.innerHTML= "Start";
+    startButton.innerHTML = "Start";
     clearInterval(interval);
     timer = [0,0,0,0];
     theTimer.innerHTML =  "00:00.00";
+    i=0;
 }
+
+// function to show the lapvalues
+lapButton.onclick = function () {
+    var lap = document.createElement('div');
+    lap.id = "notelap";
+    // var lapvalues = document.createTextNode('p');
+    // lap.appendChild(lapvalues);
+    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + "." + leadingZero(timer[2]);
+    lap.innerHTML = "Lap" +  " " + i + " " + currentTime;
+    i++;
+
+    document.getElementById("main").appendChild(lap);
+
+}
+
